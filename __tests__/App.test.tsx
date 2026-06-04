9241d60cca918e37382297444541acd61a79d37a
Syed Arham Raza <arhamraza399@gmail.com>
2026-06-04 18:28:56 +0500

    test: unmount App component in tests to prevent open handle leaks

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,7 +7,11 @@ import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
 test('renders correctly', async () => {
+  let renderer: any;
   await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+    renderer = ReactTestRenderer.create(<App />);
+  });
+  await ReactTestRenderer.act(() => {
+    renderer.unmount();
   });
 });
